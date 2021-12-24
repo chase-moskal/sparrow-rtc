ce9bc6f724b703057755ce738e7d691e947b579b
Chase Moskal <chasemoskal@gmail.com>
2021-12-23 20:26:06 -0800

    rework: randomid in hex

--- s/toolbox/random-id.ts
+++ s/toolbox/random-id.ts
@@ -1,5 +1,33 @@
 
 export function randomId(): string {
-	return Math.random().toString().split(".")[1]
-		+ Math.random().toString().split(".")[1]
+	let id = ""
+	for (let i = 0; i < 8; i++) {
+		id += randomSample(hex)
+	}
+
+	return id
 }
+
+function randomSample<X>(space: X[]) {
+	const index = Math.floor(Math.random() * space.length)
+	return space[index]
+}
+
+const hex = [
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+]
