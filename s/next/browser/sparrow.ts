8e4f989f613be06dde279a463dd40d72cc36da87
Chase Moskal <chasemoskal@gmail.com>
2024-05-07 02:42:06 -0700

    wip: more mocks and sparrow

--- s/next/browser/sparrow.ts
+++ s/next/browser/sparrow.ts
@@ -8,29 +8,50 @@ import {standardRtcConfig} from "./standard-rtc-config.js"
 import {standardDataChannels} from "../api/parts/establish_channels.js"
 import {ConnectionStatus, EstablishChannels, ReputationClaim, SocketClient} from "../types.js"
 
-export class Sparrow<Channels> {
-	static standardRtcConfig = standardRtcConfig
-	static standardDataChannels = standardDataChannels
+export type Clientizer = (claim: ReputationClaim | null) => Promise<SocketClient>
 
-	#url: string
-	#establishChannels: EstablishChannels<Channels>
-	claim: ReputationClaim | null = null
+export function realClient({
+		url,
+		rtcConfig,
+		establishChannels,
+		onChannelsReady,
+		onConnectionStatus,
+		handleConnectionClosed,
+	}: {
+		url: string
+		rtcConfig: RTCConfiguration
+		establishChannels: EstablishChannels<unknown>
+		onChannelsReady: (peer: RTCPeerConnection, channels: unknown) => void
+		onConnectionStatus: (status: ConnectionStatus) => void
+		handleConnectionClosed: () => void
+	}): Clientizer {
 
-	constructor(o: {
-			url: string
-			claim: ReputationClaim | null
-			establishChannels: EstablishChannels<Channels>
-		}) {
+	return async claim => Renraku.webSocketClient({
+		link: url,
+		timeout: 10_000,
+		handleConnectionClosed,
+		serverMetas: serverMetas(async() => {
+			if (!claim) throw new Error("no claim")
+			return {claim}
+		}),
+		clientApi: serverRemote => makeBrowserApi({
+			server: serverRemote,
+			rtcConfig,
+			onChannelsReady,
+			establishChannels,
+			onConnectionStatus,
+		}),
+	})
+}
 
-		this.#url = o.url
-		this.#establishChannels = o.establishChannels
-		this.claim = o.claim
-	}
-
+export class SparrowClient {
 	#socket: SocketClient | null = null
+	claim: ReputationClaim | null = null
 	onConnectionChange = pubsub<[]>()
 	onConnectionStatus = pubsub<[ConnectionStatus]>()
 	onChannelsReady = pubsub<[RTCPeerConnection, unknown]>()
+
+	constructor(public clientizer: Clientizer) {}
 
 	get connection() {
 		const claim = this.claim
@@ -54,32 +75,31 @@ export class Sparrow<Channels> {
 		if (connection)
 			return connection
 
-		this.#socket = await Renraku.webSocketClient({
-			link: this.#url,
-			timeout: 10_000,
-			serverMetas: serverMetas(async() => {
-				const claim = this.claim
-				if (!claim) throw new Error("no claim")
-				return {claim}
-			}),
-			handleConnectionClosed: () => {
-				this.#socket = null
-			},
-			clientApi: serverRemote => makeBrowserApi({
-				server: serverRemote,
-				rtcConfig: standardRtcConfig,
-				establishChannels: this.#establishChannels,
-				onConnectionStatus: status => this.onConnectionStatus.publish(status),
-				onChannelsReady: (peer, channels) => this.onChannelsReady.publish(peer, channels),
-			}),
-		})
-
+		this.#socket = await this.clientizer(this.claim)
 		const {remote} = this.#socket
 		const claim = await remote.v1.basic.claimReputation(this.claim)
 		this.claim = claim
 		this.onConnectionChange.publish()
 
 		return {remote, claim}
+	}
+}
+
+export class Sparrow extends SparrowClient {
+	static standardRtcConfig = standardRtcConfig
+	static standardDataChannels = standardDataChannels
+
+	constructor(o: {
+			url: string
+			rtcConfig: RTCConfiguration
+			establishChannels: EstablishChannels<unknown>
+		}) {
+		super(realClient({
+			...o,
+			onChannelsReady: (peer, connection) => this.onChannelsReady.publish(peer, connection),
+			onConnectionStatus: status => this.onConnectionStatus.publish(status),
+			handleConnectionClosed: () => { this.close() },
+		}))
 	}
 }
 
--- s/next/testing/mocks.ts
+++ s/next/testing/mocks.ts
@@ -2,71 +2,55 @@
 import * as Renraku from "renraku"
 
 import {Core} from "../core/core.js"
-import {Sparrow} from "../browser/sparrow.js"
-import {Public} from "../../toolbox/public.js"
-import {pubsub} from "../../toolbox/pubsub.js"
 import {serverMetas} from "../api/parts/metas.js"
-import {StandardDataChannels} from "../api/parts/establish_channels.js"
-import {BrowserRemote, ConnectionStatus, ReputationClaim, ServerRemote, SocketClient} from "../types.js"
+import {Clientizer, SparrowClient} from "../browser/sparrow.js"
+import {BrowserRemote, ConnectionStatus, ReputationClaim, ServerRemote} from "../types.js"
 
 export class ServerMock {
 	core = new Core()
 
 	browser(claim: ReputationClaim | null) {
-		return new SparrowMock(this.core, claim)
+		const sparrow = new SparrowMock({core: this.core})
+		sparrow.claim = claim
+		return sparrow
 	}
 }
 
-export class SparrowMock implements Public<Sparrow<StandardDataChannels>> {
-	constructor(private core: Core, public claim: ReputationClaim | null) {}
-
-	#socket: SocketClient | null = null
-	onConnectionChange = pubsub<[]>()
-	onConnectionStatus = pubsub<[ConnectionStatus]>()
-	onChannelsReady = pubsub<[RTCPeerConnection, unknown]>()
-
-	get connection() {
-		const claim = this.claim
-		const remote = this.#socket?.remote
-		return (remote && claim)
-			? {remote, claim}
-			: null
-	}
-
-	close() {
-		if (this.#socket) {
-			this.#socket.close()
-			this.#socket = null
-			this.onConnectionChange.publish()
-		}
-	}
-
-	async requireConnection() {
-		const {connection} = this
-
-		if (connection)
-			return connection
-
-		const browser = mockBrowser()
-		const socket = mockSocketConnection(browser)
-		const {handling} = this.core.connections.accept(socket)
-		const remote = (
-			Renraku.mock()
-				.forApi(handling.api)
-				.withMetas(serverMetas(async() => {
-					if (!this.claim) throw new Error("no claim")
-					return {claim: this.claim}
-				}))
-		) as ServerRemote
-
-		this.claim = await remote.v1.basic.claimReputation(this.claim)
-		this.onConnectionChange.publish()
-
-		return {remote, claim: this.claim}
+export class SparrowMock extends SparrowClient {
+	constructor(o: {core: Core}) {
+		super(mockClient({
+			...o,
+			onChannelsReady: (peer, connection) => this.onChannelsReady.publish(peer, connection),
+			onConnectionStatus: status => this.onConnectionStatus.publish(status),
+			handleConnectionClosed: () => { this.close() },
+		}))
 	}
 }
 
 ///////////////////////////////////////
+
+function mockClient(o: {
+		core: Core
+		onChannelsReady: (peer: RTCPeerConnection, channels: unknown) => void
+		onConnectionStatus: (status: ConnectionStatus) => void
+		handleConnectionClosed: () => void
+	}): Clientizer {
+
+	return async claim => {
+		const browser = mockBrowser()
+		const socket = mockSocketConnection(browser)
+		const {handling} = o.core.connections.accept(socket)
+		const remote = (
+			Renraku.mock()
+				.forApi(handling.api)
+				.withMetas(serverMetas(async() => {
+					if (!claim) throw new Error("no claim")
+					return {claim}
+				}))
+		) as ServerRemote
+		return {remote, close: () => {}}
+	}
+}
 
 function mockSocketConnection(browser: BrowserRemote): Renraku.SocketConnection {
 	return {
